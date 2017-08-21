6ef19ac2f3c8b36c722d7ddf10eecca7f8182644
Aaron Fischer <mail@aaron-fischer.net>
2017-08-21 21:15:28 +0200

    Fix the build process

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-rm -f muri-src.zip muri.zip
-uglifyjs --no-dead-code --output src/muri.min.js src/*.js
-(cd src && zip -q -9 ../muri.zip index.html muri.min.js assets/**/* vendor/*.min.js)
+rm -f muri-src.zip muri.zip src/muri.min.js
+uglifyjs --compress --mangle --no-dead-code --output src/muri.min.js src/*.js
+(cd src && zip -q -9 ../muri.zip index.html muri.min.js assets/* assets/**/* vendor/*.min.js)
 (cd src && zip -q -9 ../muri-src.zip *)
 
 filesize=`stat --printf="%s" muri.zip`
