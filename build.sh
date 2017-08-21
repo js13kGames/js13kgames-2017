409ec35c23e264c5e68275e2793f13557d8d34b8
Aaron Fischer <mail@aaron-fischer.net>
2017-08-21 21:19:21 +0200

    ASD

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 rm -f muri-src.zip muri.zip src/muri.min.js
 uglifyjs --compress --mangle --no-dead-code --output src/muri.min.js src/*.js
 (cd src && zip -q -9 ../muri.zip index.html muri.min.js assets/* assets/**/* vendor/*.min.js)
-(cd src && zip -q -9 ../muri-src.zip *)
+(cd src && zip -q -9 ../muri-src.zip ./* ./**/* ./**/**/* ../README)
 
 filesize=`stat --printf="%s" muri.zip`
 echo $filesize "byte ->" $((100*$filesize/13000)) "%"
